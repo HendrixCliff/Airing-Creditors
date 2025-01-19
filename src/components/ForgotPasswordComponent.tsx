8006ea077c43e3da87e125fd2430006e3b653a6b
Igwegbe Godwin Chukwuebuka <godwincliff10@gmail.com>
2025-01-19 22:47:10 +0100

    Time in Memoria

--- src/components/ForgotPasswordComponent.tsx
+++ src/components/ForgotPasswordComponent.tsx
@@ -4,6 +4,7 @@ import { useAppSelector } from './../hooks/useAppSelector';
 import { forgotPassword } from './../redux/fetchData';
 import { Link } from 'react-router-dom'
 import { TiArrowBack } from 'react-icons/ti';
+import { FaArrowLeftLong} from "react-icons/fa6"
 
 const ForgotPasswordComponent: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -20,9 +21,10 @@ const ForgotPasswordComponent: React.FC = () => {
 
   return (
     <section>
-      <section className="flex">
+      <Link to="/authenticate"><FaArrowLeftLong size={25}/></Link>
+      <section className="flex max-md:flex-col">
       <img className="w-[52%] max-md:w-[100%] max-md:m-[.2em] object-cover h-[38em] max-md:h-[20em]"  src="/images/tortise.webp" alt="Description" />
-      <form  className="max-md:ml-[.1em] ml-[5.5em] mt-[14em] max-md:mt-[10em]  flex flex-col">
+      <form  className="max-md:ml-[.1em] ml-[5.5em] mt-[14em] max-md:mt-[1em] max-md:mt-[10em]  flex flex-col">
       <label className="flex flex-col gap-[.5em]" >
       <h3 className="text-[1.6rem] font-semibold">Forgot Password?</h3>
       <h3 className="text-[1.4rem]">No worries, we'll send you a reset instructions.</h3>
--- src/components/resetPassword.tsx
+++ src/components/resetPassword.tsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
-import { useAppDispatch} from './../hooks/useAppDispatch';
-import { useAppSelector} from './../hooks/useAppSelector'; 
-import { resetPassword } from './../redux/fetchData'; 
+import { useAppDispatch } from './../hooks/useAppDispatch';
+import { useAppSelector } from './../hooks/useAppSelector';
+import { resetPassword } from './../redux/fetchData';
 import { clearAuthMessages } from './../redux/authSlice';
-import { Link } from 'react-router-dom' 
+import { useNavigate, Link } from 'react-router-dom';
 import { TiArrowBack } from 'react-icons/ti';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { FaArrowLeftLong } from 'react-icons/fa6';
 
 const ResetPassword = () => {
+  const navigate = useNavigate();
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-   const [showPassword, setShowPassword] = useState(false);
-    const [showPasswordAgain, setShowPasswordAgain] = useState(false)
-  const dispatch = useAppDispatch()
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordAgain, setShowPasswordAgain] = useState(false);
+
+  const dispatch = useAppDispatch();
   const { loading, successMessage, errorMessage } = useAppSelector((state) => state.auth);
 
   const togglePasswordVisibility = () => {
@@ -21,8 +24,8 @@ const ResetPassword = () => {
   };
 
   const togglePasswordVisibilityAgain = () => {
-  setShowPasswordAgain(!showPasswordAgain)
-  }
+    setShowPasswordAgain(!showPasswordAgain);
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -34,102 +37,107 @@ const ResetPassword = () => {
   };
 
   return (
-    <section className="flex ">
-       <img className="w-[56%] max-md:w-[100%] max-md:m-[.2em] object-cover h-[38em] max-md:h-[20em]"  src="/images/nature.webp"/>
-    <section  className="max-md:ml-[1em] w-[47%] ml-[8em] mt-[9em] max-md:mt-[6em] flex flex-col overflow-hidden">
-      <h2 className="text-[1.6rem] max-md:text-[1.8rem] max-md:text-center max-md:w-[70%]">Reset Password</h2>
-      <form onSubmit={handleSubmit}>
-          <label className="flex flex-col gap-[.5em] relative">
-         <h3 className="max-md:text-[1.2rem] text-[1.4rem]"> New Password </h3>
-          <input
-             type={showPassword ? 'text' : 'password'}
-            className="w-[60%] p-[.4em] max-md:w-[90%]  rounded-[.1em] border-[.2em] border-solid bg-[#f1fffc] border-[#f1fffc]"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="New Password"
-            required
-          />
-    <FontAwesomeIcon
-      icon={showPassword ? faEyeSlash : faEye}
-      onClick={togglePasswordVisibility}
-      className="flex max-md:hidden"
-      style={{
-        position: 'absolute',
-        right: '16.9em',
-        top: '79%',
-        transform: 'translateY(-50%)',
-        cursor: 'pointer',
-      }}
-    /><FontAwesomeIcon
-      icon={showPassword ? faEyeSlash : faEye}
-      onClick={togglePasswordVisibility}
-      className="hidden max-md:flex "
-      style={{
-        position: 'absolute',
-        right: '5.9em',
-        top: '68%',
-        transform: 'translateY(-50%)',
-        cursor: 'pointer',
-      }}
-    />
-      </label>
-          <label className="flex flex-col gap-[.5em] relative">
-           <h3 className="text-[1.4rem] max-md:text-[1.2rem]"> Confirm New Password </h3>
-          <input
-             type={showPasswordAgain ? 'text' : 'password'}
-              className="w-[60%] max-md:w-[90%] p-[.4em] rounded-[.1em] border-[.2em] border-solid bg-[#f1fffc] border-[#f1fffc]"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            placeholder="Confirm New Password"
-            required
-          />
-            <FontAwesomeIcon
-          icon={showPasswordAgain ? faEyeSlash : faEye}
-          onClick={togglePasswordVisibilityAgain}
-          className="flex max-md:hidden"
-          style={{
-            position: 'absolute',
-            right: '17em',
-            top: '74%',
-            transform: 'translateY(-50%)',
-            cursor: 'pointer',
-          }}
-         />
-           <FontAwesomeIcon
-          icon={showPasswordAgain ? faEyeSlash : faEye}
-          onClick={togglePasswordVisibilityAgain}
-          className="hidden max-md:flex"
-          style={{
-            position: 'absolute',
-            right: '6em',
-            top: '73%',
-            transform: 'translateY(-50%)',
-            cursor: 'pointer',
-          }}
-         />
-          </label>
-        <button type="submit"  className="text-center ml-[3em] max-md:w-[70%] mt-[1em]
-         p-[.2em] font-bold rounded-[1em] text-[#f1fffc] border-solid
-          bg-[#1f1915] max-md:mt-[1.4em] w-[30%] text-[1.4rem]" disabled={loading}>
-          {loading ? 'Loading...' : 'Reset Password'}
-        </button>
-        <Link to="/authenticate" className="flex text-[1.3rem] max-md:mt-[1.2em] items-center font-semibold">< TiArrowBack size={20}/>Back to Login</Link>
-      </form>
-      {successMessage && (
-        <div>
-          <p>{successMessage}</p>
-          <button onClick={handleClearMessages}>Clear</button>
-        </div>
-      )}
-      {errorMessage && (
-        <div>
-          <p>{errorMessage}</p>
-          <button onClick={handleClearMessages}>Clear</button>
-        </div>
-      )}
+    <section>
+      {/* Navigate Back */}
+      <section onClick={() => { handleClearMessages(); navigate(-1); }} style={{ cursor: 'pointer' }}>
+        <FaArrowLeftLong size={25} />
+      </section>
+
+      {/* Form Section */}
+      <section className="flex max-md:flex-col">
+        <img
+          className="w-[56%] max-md:w-[100%] max-md:m-[.2em] object-cover h-[38em] max-md:h-[20em]"
+          src="/images/nature.webp"
+          alt="Nature"
+        />
+        <section className="max-md:ml-[1em] max-md:w-[100%] w-[44%] max-md:mt-[1em] ml-[8em] mt-[9em] flex flex-col overflow-hidden">
+          <h2 className="text-[1.6rem] max-md:text-[1.8rem] max-md:text-center max-md:w-[70%]">
+            Reset Password
+          </h2>
+          <form onSubmit={handleSubmit}>
+            {/* New Password */}
+            <label className="flex flex-col gap-[.5em] relative">
+              <h3 className="max-md:text-[1.2rem] text-[1.4rem]">New Password</h3>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                className="w-[80%] p-[.4em] max-md:w-[90%] rounded-[.1em] border-[.2em] border-solid bg-[#f1fffc] border-[#f1fffc]"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="New Password"
+                aria-label="New Password"
+                required
+              />
+              <FontAwesomeIcon
+                icon={showPassword ? faEyeSlash : faEye}
+                onClick={togglePasswordVisibility}
+                className="absolute flex max-md:hidden right-[9em] top-[80%] transform -translate-y-1/2 cursor-pointer"
+              />
+              <FontAwesomeIcon
+                icon={showPassword ? faEyeSlash : faEye}
+                onClick={togglePasswordVisibility}
+                className="absolute hidden max-md:flex right-[5em] top-[70%] transform -translate-y-1/2 cursor-pointer"
+              />
+            </label>
+
+            {/* Confirm Password */}
+            <label className="flex flex-col gap-[.5em] relative">
+              <h3 className="text-[1.4rem] max-md:text-[1.2rem]">Confirm New Password</h3>
+              <input
+                type={showPasswordAgain ? 'text' : 'password'}
+                className="w-[80%] max-md:w-[90%] p-[.4em] rounded-[.1em] border-[.2em] border-solid bg-[#f1fffc] border-[#f1fffc]"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                placeholder="Confirm New Password"
+                aria-label="Confirm Password"
+                required
+              />
+              <FontAwesomeIcon
+                icon={showPasswordAgain ? faEyeSlash : faEye}
+                onClick={togglePasswordVisibilityAgain}
+                className="absolute flex max-md:hidden right-[9em] top-[80%] transform -translate-y-1/2 cursor-pointer"
+              />
+               <FontAwesomeIcon
+                icon={showPasswordAgain ? faEyeSlash : faEye}
+                onClick={togglePasswordVisibilityAgain}
+                className="absolute hidden max-md:flex right-[5em] top-[75%] transform -translate-y-1/2 cursor-pointer"
+              />
+            </label>
+
+            {/* Submit Button */}
+            <button
+              type="submit"
+              className="text-center ml-[6em] max-md:ml-[5em] max-md:w-[50%] mt-[1em] p-[.2em] font-bold rounded-[1em] text-[#f1fffc] border-solid bg-[#1f1915] max-md:mt-[1.4em] w-[30%] text-[1.4rem]"
+              disabled={loading}
+            >
+              {loading ? 'Loading...' : 'Reset Password'}
+            </button>
+
+            {/* Back to Login Link */}
+            <Link
+              to="/authenticate"
+              className="flex text-[1.3rem] max-md:mt-[1.2em] items-center font-semibold"
+            >
+              <TiArrowBack size={20} />
+              Back to Login
+            </Link>
+          </form>
+
+          {/* Success and Error Messages */}
+          {successMessage && (
+            <div>
+              <p>{successMessage}</p>
+              <button onClick={handleClearMessages}>Clear</button>
+            </div>
+          )}
+          {errorMessage && (
+            <div>
+              <p>{errorMessage}</p>
+              <button onClick={handleClearMessages}>Clear</button>
+            </div>
+          )}
+        </section>
+      </section>
     </section>
-    </section>
-   
   );
 };
 
