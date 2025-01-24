0ba5a8fe43d75dc5daf2ce1aa276dd8656b38d41
Igwegbe Godwin Chukwuebuka <godwincliff10@gmail.com>
2025-01-24 19:43:57 +0100

    Display

--- src/protectedRoutes/PaymentPage.tsx
+++ src/protectedRoutes/PaymentPage.tsx
@@ -310,7 +310,7 @@ const handleCardDetails = (e: React.ChangeEvent<HTMLInputElement>) => {
         {/* Submit Button */}
         <button
           type="submit"
-          className="col-span-2 bg-[#c8a7ff] text-white max-[600px]:w-[50%] max-[550px]:w-[60%] max-w-[50%] font-bold text-lg rounded-[.2em]  mx-[auto]"
+          className="col-span-2 bg-[#c8a7ff] text-white max-[600px]:w-[50%] max-[550px]:w-[60%] max-w-[50%] font-bold max-md:text-md text-lg rounded-[.2em]  mx-[auto]"
           disabled={
             !token ||
             loading ||
